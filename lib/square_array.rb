cae2b7c2939ddd35729dc117d68d86b89b733839
Patrick Alexander <patrick_alexander@mail.com>
2019-08-30 21:03:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,11 @@ def square_array(numbers)
   
   while counter < numbers.length do
     squared_numbers << numbers[counter] * numbers[counter]
+<<<<<<< HEAD
     counter +=1
+=======
+    counter =+1
+>>>>>>> 5f95e44ffdd7825bdc529b90bcaf906d4f79de2d
   end
   squared_numbers
 end
